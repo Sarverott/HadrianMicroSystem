aecccd894e89d00311a4be946d0c2dceec17358a
Adam Borucki <sarverott@gmail.com>
2018-04-09 01:11:30 +0200

    basic file of project

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*
+**
+** JanosMicroSystem v1.2
+** Adam Borucki (Sarverott) 2018
+** https://github.com/Sarverott
+** Program for Arduino, to connect with computer and read orders, expressed in commands
+*/
 int allowed_pins[8]={2,3,4,5,6,7,8,9};
 void setup(){
   for(int i=0;i<sizeof(allowed_pins);i++){

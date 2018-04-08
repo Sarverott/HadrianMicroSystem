734e598ee1127099fea520bebe1c0cb3d13d26bb
Adam Borucki <sarverott@gmail.com>
2018-04-09 01:05:07 +0200

    basic code of Janos
    this is main version, prepared to cooperate with server applications

---
+++ main.c
@@ -0,0 +1,40 @@
+int allowed_pins[8]={2,3,4,5,6,7,8,9};
+void setup(){
+  for(int i=0;i<sizeof(allowed_pins);i++){
+    pinMode(allowed_pins[i], OUTPUT);
+  }
+  Serial.begin(9600);
+  Serial.print("#");
+}
+void loop(){
+  String inputCommand[10]={"","","","","","","","","",""};
+  int argIndex=0;
+  while(true){
+    if(Serial.available()){
+      char inChar=Serial.read();
+      if(inChar==':'){
+        break;
+      }else{
+        if(inChar=='-'){
+          argIndex++;
+          continue;
+        }else{
+          inputCommand[argIndex]+=inChar;
+        }
+      }
+    }else{
+      delay(10);
+    }
+  }
+  if(inputCommand[0]=="pin"){
+    for(int i=0;i<sizeof(allowed_pins);i++){
+      if(allowed_pins[i]==(inputCommand[1]).toInt()){
+        if(inputCommand[2]=="high"){
+          digitalWrite(inputCommand[1].toInt(), HIGH);
+        }else if(inputCommand[2]=="low"){
+          digitalWrite(inputCommand[1].toInt(), LOW);
+        }
+      }
+    }
+  }
+}
